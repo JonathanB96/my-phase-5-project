952e2b6298da5312be78915c71eee020595980ec
jonathan bola <bolajonathan@yahoo.com>
2022-10-12 11:09:26 -0500

    model relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+    belongs_to :category
+    belongs_to :user
+    belongs_to :cuisine
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :recipes
+    has_many :categories, through: :recipes
+    has_many :cuisines, through: :recipes
 end
