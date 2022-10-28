4273ffab1ef454a7891c7f0db4347ac8d114e881
jonathan bola <bolajonathan@yahoo.com>
2022-10-28 16:14:17 -0500

    CRUD actions for Recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,75 @@
 class RecipesController < ApplicationController
-
-    def index 
+    rescue_from  ActiveRecord::RecordNotFound, with: :not_found_response
+    def index
         recipes = Recipe.all 
+
         render json: recipes
+    end
+
+    def show
+       
+        recipe = Recipe.find(params[:id])
+        
+
+        render json: recipe,  status: :ok
+    
+    end  
+
+    def create 
+
+        user = User.find_by(id: session[:user_id])
+        if user
+            # category = Category.find_by(name: params[:category])      
+            # recipe = category.recipes.create(recipe_params) 
+            # render json: recipe, status: :created 
+            recipe = Recipe.create(name: params[:name], 
+            steps: params[:steps], image_url: params[:image_url],
+            category_id: Category.find_by("name LIKE ?", params[:category]).id, 
+            user_id: params[:user_id],
+            cuisine_id: Cuisine.find_by("name LIKE ?", params[:cuisine]).id)
+            render json: recipe, status: :created      
+            
+        else
+            render json: {errors: ["unauthorized"]}, status: :unauthorized   
+        end 
+        
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        if user
+            recipe = user.recipes.find(params[:id])
+            recipe.destroy
+            head  :no_content 
+            
+        else
+            render json: {errors: ["unauthorized"]}, status: :unauthorized   
+        end 
+
+    end
+
+
+    def update 
+        user = User.find_by(id: session[:user_id])
+        if user
+            recipe = user.recipes.find(params[:id])
+            recipe.update(recipe_params)
+           render json: recipe
+            
+        else
+            render json: {errors: ["unauthorized"]}, status: :unauthorized   
+        end 
+        
+
+    end
+
+    private
+    
+    def recipe_params
+      params.permit(:name, :steps, :image_url, :user_id)
+    end
+
+    def not_found_response
+        render json: {error: "not found"}, status: :not_found
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,16 @@ cuisines = ['American', 'Global', 'Cuisine', 'Italian', 'Vegetarian', 'Asian', '
 cuisines = cuisines.each do |cuisine|
     Cuisine.create(name: cuisine)
 end
-    
-Recipe.create(
+
+ 
+ Recipe.create(
     name: "Easy Salsa Sloppy Joes",
     steps: "In a large skillet, cook beef over medium heat until no longer pink, breaking into crumbles; drain. Stir in salsa, soup and brown sugar. Cover and simmer 10 minutes or until heated through. Serve on buns.",
     image_url: "https://drive.google.com/uc?id=1B3Tza3a5H69qH1fPLHkz8m-hx2c6TrsD",
     category_id: 2,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 1
+
  )
  Recipe.create(
     name: "Cheesy Hash Brown Bake",
@@ -54,7 +57,7 @@ Recipe.create(
     image_url:"https://drive.google.com/uc?id=1ocOm__U7moQQCEtCHAIpBtCsm1mxR8Ms",
     category_id: 5,
     user_id: rand(1..5),
-    cuisine_id: 1
+    cuisine_id: 2
  )
  Recipe.create(
     name: "Shorcut Split Pea Soup",
@@ -62,7 +65,7 @@ Recipe.create(
     image_url: "https://drive.google.com/uc?id=11q-LyUZiwGjEhtflVRnkxuCOSfzz-nfk",
     category_id: 3,
     user_id: rand(1..5),
-    cuisine: 2
+    cuisine_id: 2
  )
  Recipe.create(
     name: "Tasty Onion Chicken",
@@ -70,40 +73,45 @@ Recipe.create(
     image_url: "https://drive.google.com/uc?id=1SU9exencf_e1-RP18wtrXVFzSBcOYu62",
     category_id: 3,
     user_id: rand(1..5),
-    cuisine_id: 
+    cuisine_id: 1
  )
  Recipe.create(
     name: "Quick White Chili",
     steps: "In a large saucepan, combine the first 6 ingredients. Bring to a boil over medium-high heat; remove from the heat. Add cheese and cilantro; stir until cheese is melted. If desired, serve with additional sour cream and cilantro.",
     image_url: "https://drive.google.com/uc?id=1iROVYtvC8hTwWPa02mQdJY4eCQzZhgp1",
     category_id: 3,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 7
  )
  Recipe.create(
     name: "Creamy Coleslaw",
     steps: "Place coleslaw mix in a large bowl. In a small bowl, combine the remaining ingredients; stir until blended. Pour over coleslaw mix and toss to coat. Refrigerate until serving.",
     image_url: "https://drive.google.com/uc?id=1CunxCoho7GpSKU4hhmdtGbic4456Nf0M",
     category_id: 4,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 2
  )
  Recipe.create(
     name: "Enchila Chicken Soup",
     steps: "In a large saucepan, combine the soups, milk, chicken, enchilada sauce and chiles. Cook and stir over medium heat until heated through. Serve with sour cream.",
     image_url:  "https://drive.google.com/uc?id=14UCIX_h27VjWP0LLfS3Pb1zQRVaQPVPV",
     category_id: 3,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 7
  )
  Recipe.create(
     name: "One-pot Mac and cheese ",
     steps: "In a Dutch oven, combine milk, water and macaroni; bring to a boil over medium heat. Reduce heat and simmer until macaroni is tender and almost all the cooking liquid has been absorbed, 12-15 minutes, stirring frequently. Reduce heat to low; stir in cheeses until melted. Season with salt and pepper.",
     image_url: "https://drive.google.com/uc?id=1MTeQAMtKPNW-rPsJKc4_4RFZmO7YS4KD",
     category_id: 2,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 4
  )
  Recipe.create(
-    name: "Flavorful Pot Rost",
+    name: "Flavorful Pot Roast",
     steps: "Place the chuck roasts in a 5-qt. slow cooker. In a small bowl, combine the salad dressings and gravy mix; stir in water. Pour over meat. Cover and cook on low for 7-8 hours or until tender. If desired, sprinkle with parsley and thicken cooking juices for gravy.",
     image_url: "https://drive.google.com/uc?id=1ObZ3UvX8FTmAtypjDQUW3QfYJ0jxKQey",
     category_id: 3,
-    user_id: rand(1..5)
+    user_id: rand(1..5),
+    cuisine_id: 2
  )
