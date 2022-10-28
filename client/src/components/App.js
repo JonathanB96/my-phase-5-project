3bb3fd93c77528985bcd58b89ef91c363d1ec3e3
jonathan bola <bolajonathan@yahoo.com>
2022-10-28 16:01:12 -0500

    setting up react routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import NavBar from './NavBar';
+import Signup from './Signup';
+import NewRecipeForm from './NewRecipeForm';
+import Login from './Login';
+import RecipeList from './RecipeList';
+import Tips from './Tips';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <NavBar />
+        <Switch>
+          <Route exact path="/recipes">
+            <RecipeList />
+          </Route>
+          <Route exact path="/signup">
+            <Signup/>
+          </Route>
+          <Route exact path="/login">
+            <Login/>  
+          </Route>
+          <Route exact path="/addRecipe">
+           <NewRecipeForm/>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+     
     </div>
   );
 }
