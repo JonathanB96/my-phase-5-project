b78cce7029fc6e1f26da79cf454829559802e691
jonathan bola <bolajonathan@yahoo.com>
2022-11-28 10:24:09 -0600

    creating favorites table and controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
     belongs_to :category
     belongs_to :user
     belongs_to :cuisine
+    has_many :favorites 
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :categories, through: :recipes
     has_many :cuisines, through: :recipes
+    has_many :favorites
     has_secure_password
 end
