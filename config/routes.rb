25940458307278e4e598409d65fa4fb451db6450
jonathan bola <bolajonathan@yahoo.com>
2022-12-26 15:33:02 -0600

    client side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
- 
-  resources :recipes
-  resources :categories, only: [:index]
-  resources :cuisines, only: [:index]
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  get '/users', to: 'users#index'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  post '/:id/:recipe_id', to: 'users#create_favorite'
-  get 'pages/download', to: 'pages#download'
-  post '/favorites', to: 'favorites#create'
-  get '/favorites', to: 'favorites#index'
-  delete '/favorites/:id', to: 'favorites#destroy'
+  namespace :api do
+    resources :recipes
+    resources :categories, only: [:index]
+    resources :cuisines, only: [:index]
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
+    get '/users', to: 'users#index'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    post '/:id/:recipe_id', to: 'users#create_favorite'
+    get 'pages/download', to: 'pages#download'
+    post '/favorites', to: 'favorites#create'
+    get '/favorites', to: 'favorites#index'
+    delete '/favorites/:id', to: 'favorites#destroy'
+  end
+  
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
